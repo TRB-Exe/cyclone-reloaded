fa5c6b1620b6cbbfda05b028f89e5a952961f011
whiteless.exe <72357112+TRB-Exe@users.noreply.github.com>
2021-09-30 12:19:11 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+﻿const { Client } = require('yuuko');
+const path = require('path');
+const dotenv = require('dotenv');
+var env = dotenv.config();
+env = process.env;
+
+// импорт токена и префикса с файла .env
+
+const bot = new Client({
+    token: env.TOKEN,
+    prefix: env.PREFIX,
+    ignoreBots: true,
+});
+
+
+bot.extendContext({
+    variableOne: 'variable1',
+});
+bot.editStatus("idle", {name: `${env.PREFIX}help | v${env.VERSION}`, type: 3})
+
+bot.on('error', (err) => {
+    console.error(err);
+});
+
+bot.globalCommandRequirements = {
+    guildOnly: true,
+};
+
+bot
+    .addDir(path.join(__dirname, 'commands'))
+    .addDir(path.join(__dirname, 'events'))
+    .connect();
